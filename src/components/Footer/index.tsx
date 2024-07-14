6cd3b079f9bb7c922a775cd80ef886299e9f0475
雨希 张 <2512061959@qq.com>
2024-07-14 17:31:27 +0800

    umi+ts

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,32 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+const Footer: React.FC = () => {
+  const defaultMessage = '版权归zyx所有';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'Ant Design Pro',
+          title: 'Ant Design Pro',
+          href: 'https://pro.ant.design',
+          blankTarget: true,
+        },
+        {
+          key: 'Ant Design',
+          title: 'Ant Design',
+          href: 'https://ant.design',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <><GithubOutlined />zyx的Github</>,
+          href: 'https://github.com/hc-21zyx',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
